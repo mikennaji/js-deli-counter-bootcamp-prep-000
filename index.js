32b87c6c49029751c4670a328f4b6f48fbfe2f77
Michael Nnaji <mnnaji3@uwo.ca>
2019-12-26 14:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(katzDeliLine){
   }
     else if(katzDeliLine.length>=1){
     var currentpes
-    return("Currently serving "+katzDeliLine.pop(katzDeliLine[0])+".")
+    return("Currently serving "+katzDeliLine.shift()+".")
 
     }
 }
@@ -20,4 +20,6 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length===0){
   return("The line is currently empty.")
   }
+    else if(katzDeliLine.length>=1){}
+    return(`The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}, 3. ${katzDeliLine[2]}`)
 }
